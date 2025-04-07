1029259312bbf3aa982ccbc87cfec0525a1c1a7f
SI0209 <suh.ism0209@gmail.com>
2025-04-07 19:00:34 +0200

    Testing MDI Concept

--- DrivingSchoolBookingSystem/Form5.Designer.cs
+++ DrivingSchoolBookingSystem/Form5.Designer.cs
@@ -94,10 +94,10 @@
             this.panel1.Controls.Add(this.textBox3);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(186, 2);
+            this.panel1.Location = new System.Drawing.Point(185, 2);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1561, 838);
+            this.panel1.Size = new System.Drawing.Size(1154, 838);
             this.panel1.TabIndex = 0;
             // 
             // dgvVehicle
