80796b2627d056fb41097b7ea1c5fb2e1109df3b
Michaela Chetty <223011254@stu.ukzn.ac.za>
2025-06-16 10:55:53 +0200

    Progress form

--- DrivingSchoolBookingSystem/TrackLearnerProgress.cs
+++ DrivingSchoolBookingSystem/TrackLearnerProgress.cs
@@ -84,7 +84,7 @@ namespace DrivingSchoolBookingSystem
             textBox1.Enabled = false;
             textBox2.Enabled = false;
             textBox3.Enabled = false;
-            button2.Visible = false; // Disable Update button when selecting a learner from the new learners list
+            
 
             dateTimePicker1.Value = DateTime.Now;
             comboBox1.SelectedIndex = -1;
@@ -401,7 +401,7 @@ namespace DrivingSchoolBookingSystem
             textBox1.Enabled = false;
             textBox2.Enabled = false;
             textBox3.Enabled = false;
-            button1.Visible = false; // Disable Add button when editing
+           
 
 
         }
