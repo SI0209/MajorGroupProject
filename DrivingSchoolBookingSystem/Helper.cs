fa829ed08798b99416deffcbe2c2270e390d0119
Suhail Ismail <suh.ism0209@gmail.com>
2025-10-21 12:29:55 +0200

    Sorting out Power BI

---
+++ DrivingSchoolBookingSystem/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using Newtonsoft.Json.Linq;
+
+namespace DrivingSchoolBookingSystem
+{
+    internal class Helper
+    {
+        public class PowerBIService
+        {
+            private static string tenantId = "dbccab94-1921-4640-97e9-429462572e7b";
+            private static string clientId = "677a8dfb-c7ea-483d-9aa3-b753fdb9a5c4";
+            private static string clientSecret = "751931b2-db2d-439c-9e17-32b88b140a04";
+        }
+    }
+}
